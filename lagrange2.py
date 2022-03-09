eae7c8d4882d3f93f4a2c6cc05866196333e1e78
Julian Joaquin <jnjoaquin@shaw.ca>
2022-03-08 23:27:37 -0600

    Reivision process. Plotting Algorithm is resolved.

--- lagrange2.py
+++ lagrange2.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 G = 6.67408e-11
-m1 = 1.9885e30 # kg
-m2 = 5.972e24 # kg
-R = 1.49598e8 # km
-w = 1.992e-7 # radial velocity
+m1 = 5.972e24 # 1.9885e30 # kg
+m2 = 7.35e22 #5.972e24 # kg
+R = 3.844e8 #1.49598e11 # m
+radial_velocity = np.sqrt(G*(m1+m2)/R**3)
 
 def LN(r):
     f1 = -G*m1/((r)*abs(r))
@@ -17,7 +17,8 @@ def LN(r):
     return ang_v + f1 + f2
 
 
-L2 = root_scalar(LN, bracket=[1.50e8, 1.52e8]).root
+#L2 = root_scalar(LN, bracket=[1.50e11, 1.52e11]).root
+L2 = root_scalar(LN, bracket=[0.448e9, 0.45e9]).root
 
 def solve_lde(f, variable):
     ...
@@ -31,7 +32,7 @@ def r2(x,y,z):
     return np.sqrt((x-R)**2 + y**2 + z**2)
 
 
-def EOM(t, X):
+def nonlinear_EOM(t, X):
     Xdot = np.zeros(len(X))
     Xdot[:3] = X[3:6]
     x = X[0]
@@ -41,8 +42,10 @@ def EOM(t, X):
     ydot = X[4]
     R1 = r1(x,y,z)
     R2 = r2(x,y,z)
-    xddot = x*w**2 + 2*w*ydot - G*m1*x/(R1**3) - G*m2*(x-R)/(R2**3)
-    yddot = y*w**2 - 2*w*xdot - G*m1*y/(R1**3) - G*m2*y/(R2**3)
+    xddot = x*radial_velocity**2 + 2*radial_velocity*ydot - G*m1*x/(R1**3) \
+        - G*m2*(x-R)/(R2**3)
+    yddot = y*radial_velocity**2 - 2*radial_velocity*xdot - G*m1*y/(R1**3) \
+        - G*m2*y/(R2**3)
     zddot = - G*m1*z/(R1**3) - G*m2*z/(R2**3)
     Xdot[3] = xddot
     Xdot[4] = yddot
@@ -50,10 +53,40 @@ def EOM(t, X):
     return Xdot
 
 
+def init_vy(lpoint, distance):
+    x= lpoint + distance
+    a = x*G*(m1+m2)/(R**3) - x*G*m1/(x**3) - G*m2*(R-x)/(x**3)
+    print(a)
+    return np.sqrt(a*distance)
+
+def main2():
+    fig, ax = plt.subplots()
+    x = np.arange(-2*R, 2*R, 10000)
+    y = LN(x)
+    ax.plot(x,y)
+    ax.set_ylim(-0.01, 0.01)
+    plt.show()
+
 def main():
-    CONDITION_0 = np.array([L2 + 100/R, 0, 0, 0, 0, 0])
-    EOMs = EOM
-    
+    ixdot = 0
+    iydot = 0.0014777#init_vy(L2, 1000) # ms-1
+    print(init_vy(L2, 100))
+    CONDITION_0 = np.array([L2 - 100, 0, 0, ixdot, iydot, 0])
+    EOMs = nonlinear_EOM
+    traj = solve_ivp(EOMs, [0,28*24*3600], CONDITION_0, atol=0.000001, rtol=3e-14)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot(traj.y[0,:], traj.y[1,:], traj.y[2,:], 'b')
+    ax.plot(L2, 0, 0, 'k+')
+    ax.plot(L2-100, 0, 0, 'r+')
+
+    bound = 500
+    ax.axes.set_xlim3d(left=L2 - bound, right=L2 + bound)
+    ax.axes.set_ylim3d(bottom=-bound, top=bound)
+    ax.axes.set_zlim3d(bottom=-bound, top=bound)
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
